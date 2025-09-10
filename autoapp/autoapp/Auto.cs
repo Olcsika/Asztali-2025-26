de198ece7aee555b44b445565ff3b9a973af1470
Lébár Olivér <lebar.oliver.21@ady-nagyatad.hu>
2025-09-10 13:17:42 +0200

    a

---
+++ autoapp/autoapp/Auto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autoapp
+{
+    internal class Auto
+    {
+        public int sorszam;
+        public string marka;
+        public string modell;
+        public int gyartasev;
+        public string szin;
+        public int eladottdbszam;
+        public int atlageladar;
+
+        public Auto(string sor)
+        {
+            string[] vag = sor.Split(';');
+            this.sorszam = int.Parse(vag[0]);
+            this.marka = vag[1];
+            this.modell = vag[2];
+            this.gyartasev = int.Parse(vag[3]);
+            this.szin = vag[4];
+            this.eladottdbszam = int.Parse(vag[5]);
+            this.atlageladar = int.Parse(vag[6]);
+        }
+    }
+}
---
+++ autoapp/autoapp/Program.cs
@@ -0,0 +1,56 @@
+﻿using autoapp;
+using System.Net.Http.Headers;
+
+List<Auto> adatok = new List<Auto>();
+string[] tomb = File.ReadAllLines("autok.csv");
+foreach (string sor in tomb.Skip(1))
+{
+    adatok.Add(new Auto(sor));
+
+}
+Console.WriteLine("5. feladat: {0} db autót tároltunk el az állományban.",adatok.Count);
+int eladottosz = 0;
+int db = 0;
+foreach (Auto sor in adatok)
+{
+   if(sor.eladottdbszam != 0)
+    {
+        eladottosz += sor.eladottdbszam;
+        db++;
+    }
+
+}
+int ossz = eladottosz / db;
+Console.WriteLine("6. feladat: Átlagosan {0} db autót adtak el.", Math.Round((double)ossz), 1);
+Console.WriteLine("7. feladat: Elmúlt 5 évben eladott járművek");
+
+int ev = DateTime.Now.Year - 1;
+int eladott = ev - 5;
+foreach (Auto sor in adatok)
+{
+    if(sor.gyartasev >= eladott)
+    {
+        Console.WriteLine(" - " + sor.marka + " " + sor.modell + " " + sor.gyartasev);
+    }
+    
+}
+Console.WriteLine("8. feladatat: Legsikeresebb márkák listája az eladott darabszám alapján.");
+Dictionary<string, int> utso = new Dictionary<string, int>();
+foreach (Auto sor in adatok)
+{
+    if (sor.eladottdbszam != 0)
+    {
+        if (utso.ContainsKey(sor.marka))
+        {
+           utso[sor.marka] += sor.eladottdbszam;
+        }
+        else
+        {
+            utso.Add(sor.marka, sor.eladottdbszam);
+        }
+    }
+}
+
+foreach (var e in utso.OrderByDescending(x =>x.Value)){
+    Console.WriteLine("- {0} {1} db ", e.Key,e.Value);
+}
